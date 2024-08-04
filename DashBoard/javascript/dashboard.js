199fc6c774213765dafda5e0f698f21865b70123
Dereck Estrada Molina <dereckdem17@gmail.com>
2024-08-03 20:39:31 -0500

    Grupal

---
+++ DashBoard/javascript/dashboard.js
@@ -0,0 +1,39 @@
+var arregloJson=[
+    {
+        'nombre':'VmtDev',
+        'apellido':'BootCamp FullStack',
+        'valoracion': 10,
+        'infoAdicional':{
+            'duracion':'360h',
+            'materias':14
+        },
+        'docentes':[
+            {
+                'nombre':'Jose Moran',
+                'materia':'c sharp'
+            },
+            {
+                'nombre':'Maria Isabel Galarza',
+                'materia':'Sql Server'
+            },
+            {
+                'nombre':'Arturo Delgado',
+                'materia':'.net core'
+            }
+        ]
+    },
+    {
+        'nombre':'VmtDev',
+        'apellido':'BootCamp DataScience',
+        'valoracion': 10
+    },
+    
+]
+console.log(arregloJson)
+function obtenerTextSearch(){
+     let valbuscar=document.getElementById("buscar");
+     window. alert(valbuscar.value);
+}
+ function saludo(){
+    var hola="hola"
+}
